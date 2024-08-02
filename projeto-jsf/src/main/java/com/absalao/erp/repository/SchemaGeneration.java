db4adf8c8fa0a65b770a905a9020af6ffc850ee8
Leonardo Absalão <100871641+leoabsalao@users.noreply.github.com>
2024-08-02 17:49:59 -0300

    Entidades criadas e JPA persistence.xml

---
+++ projeto-jsf/src/main/java/com/absalao/erp/repository/SchemaGeneration.java
@@ -0,0 +1,27 @@
+package com.absalao.erp.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.absalao.erp.model.Empresa;
+
+public class SchemaGeneration {
+	
+	public static void main() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("AlgaWorksPU");
+		EntityManager em = emf.createEntityManager();
+		
+		List<Empresa> lista = em.createQuery("from Empresa", Empresa.class)
+				.getResultList();
+		
+		System.out.println(lista);
+		
+		em.close();
+		emf.close();	
+		
+	} 
+
+}
