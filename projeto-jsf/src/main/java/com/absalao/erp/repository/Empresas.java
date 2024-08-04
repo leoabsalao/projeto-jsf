d8150d3b01216106060b306d88db786388166170
Leonardo Absalão <100871641+leoabsalao@users.noreply.github.com>
2024-08-04 00:06:03 -0300

    Criado a camada de repository da classe RamoAtividade

--- projeto-jsf/src/main/java/com/absalao/erp/repository/Empresas.java
+++ projeto-jsf/src/main/java/com/absalao/erp/repository/Empresas.java
@@ -25,7 +25,8 @@ public class Empresas implements Serializable {
 	}
 	
 	public List<Empresa> pesquisar(String nome){
-		TypedQuery<Empresa> query = manager.createQuery("from Empresas where nomeFantasia like :nomeFantasia ", Empresa.class);
+		String jpql = "from Empresas where nomeFantasia like :nomeFantasia ";
+		TypedQuery<Empresa> query = manager.createQuery(jpql, Empresa.class);
 		query.setParameter("nomeFantasia", nome + "%");
 		return query.getResultList();
 	}
