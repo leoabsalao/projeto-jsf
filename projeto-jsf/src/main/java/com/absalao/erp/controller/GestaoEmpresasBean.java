48d13816526516e5f66c2e24e86127b9a77498f2
Leonardo Absalão <100871641+leoabsalao@users.noreply.github.com>
2024-08-13 16:49:55 -0300

    Implementado a validação dos botoes de editar e excluir quando uma linha é selecionada

--- projeto-jsf/src/main/java/com/absalao/erp/controller/GestaoEmpresasBean.java
+++ projeto-jsf/src/main/java/com/absalao/erp/controller/GestaoEmpresasBean.java
@@ -110,4 +110,13 @@ public class GestaoEmpresasBean implements Serializable {
 		return empresa;
 	}
 	
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+	
+	public boolean isEmpresaSelecionada() {
+		return empresa != null && empresa.getId() != null;
+	}
+		
+	
 }
